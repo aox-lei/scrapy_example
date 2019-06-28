153adaf719937c016db7c7df9f816458f632e120
aox.lei <2387813033@qq.com>
2019-06-28 17:31:16 +0800

    完成第一章 第一节 demo

--- chapter 1/section 1/house_spider/house_spider/items.py
+++ chapter 1/section 1/house_spider/house_spider/items.py
@@ -12,3 +12,11 @@ class HouseSpiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class FangSpiderEsfListItem(scrapy.Item):
+    '''
+    房天下二手房列表页Item
+    '''
+    title = scrapy.Field() # 标题
+    total_price = scrapy.Field() # 总价
+    url = scrapy.Field() # 详情页url
