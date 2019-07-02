d7db246afcfbc0f2ff86d3a190717272e38e31ea
aox.lei <2387813033@qq.com>
2019-07-02 16:55:53 +0800

    完成部分第三节代码

---
+++ chapter 1/section 3/house_spider/house_spider/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8-*-
+from mongoengine import connect
+
+connect('house')
---
+++ chapter 1/section 3/house_spider/house_spider/spiders/FangSpider.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from house_spider.items import HouseItem
+from scrapy.linkextractors import LinkExtractor
+
+
+class FangspiderSpider(scrapy.Spider):
+    name = 'FangSpider'
+    allowed_domains = ['fang.com']
+
+    def start_requests(self):
+        yield scrapy.Request('https://esf.fang.com/',
+                             callback=self.parse,
+                             headers={
+                                 'referer': 'https://www.fang.com',
+                             },
+                             dont_filter=True)
+
+    def parse(self, response):
+        links = LinkExtractor(allow=('\/chushou\/(.*)\.htm', ))
+        links = links.extract_links(response)
+        for info in links:
+            yield scrapy.Request(info.url,
+                                 callback=self.parse_info,
+                                 dont_filter=True)
+
+    def parse_info(self, response):
+        item = HouseItem()
+        item['title'] = response.css('h1.title::text').get().strip()
+        item['total_price'] = response.css(
+            'div.price_esf i::text').get().strip()
+        item['area'] = response.css(
+            'div.tab-cont.clearfix > div.tab-cont-right > div:nth-child(2) > div.trl-item1.w182 > div.tt::text'
+        ).get()
+        item['direction'] = response.css(
+            'div.tab-cont.clearfix > div.tab-cont-right > div:nth-child(3) > div.trl-item1.w146 > div.tt::text'
+        ).get().strip()
+        item['community'] = response.css(
+            'div.tab-cont.clearfix > div.tab-cont-right > div:nth-child(4) > div:nth-child(1) > div.rcont a::text'
+        ).get()
+        yield item
