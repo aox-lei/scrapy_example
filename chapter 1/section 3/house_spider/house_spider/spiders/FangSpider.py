4783208dbbf1974edf2f8837fc8b0bd8310b02e8
aox.lei <2387813033@qq.com>
2019-07-03 08:53:35 +0800

    完成第三节的方法

--- chapter 1/section 3/house_spider/house_spider/spiders/FangSpider.py
+++ chapter 1/section 3/house_spider/house_spider/spiders/FangSpider.py
@@ -39,3 +39,38 @@ class FangspiderSpider(scrapy.Spider):
             'div.tab-cont.clearfix > div.tab-cont-right > div:nth-child(4) > div:nth-child(1) > div.rcont a::text'
         ).get()
         yield item
+
+
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from house_spider.items import HouseItem
+from scrapy.linkextractors import LinkExtractor
+
+
+class FangSpider(CrawlSpider):
+    name = 'FangCrawlSpider'
+    allowed_domains = ['fang.com']
+    start_urls = ['https://esf.fang.com']
+    rules = (Rule(LinkExtractor(allow=('chushou\/(.*)\.htm', )),
+                  callback='parse_info'), )
+    custom_settings = {
+        'DEFAULT_REQUEST_HEADERS': {
+            'referer': 'https://www.fang.com',
+        }
+    }
+
+    def parse_info(self, response):
+        item = HouseItem()
+        item['title'] = response.css('h1.title::text').get().strip()
+        item['total_price'] = response.css(
+            'div.price_esf i::text').get().strip()
+        item['area'] = response.css(
+            'div.tab-cont.clearfix > div.tab-cont-right > div:nth-child(2) > div.trl-item1.w182 > div.tt::text'
+        ).get()
+        item['direction'] = response.css(
+            'div.tab-cont.clearfix > div.tab-cont-right > div:nth-child(3) > div.trl-item1.w146 > div.tt::text'
+        ).get().strip()
+        item['community'] = response.css(
+            'div.tab-cont.clearfix > div.tab-cont-right > div:nth-child(4) > div:nth-child(1) > div.rcont a::text'
+        ).get()
+        yield item
