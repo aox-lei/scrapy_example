f92269abd5dc03d700aef0f7080f2a61acdb28d7
aox.lei <2387813033@qq.com>
2019-08-08 17:00:47 +0800

    破解房天下反爬机制

---
+++ chapter 2/section 4/house_spider/house_spider/middlewares.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+from scrapy import signals
+from furl import furl
+from scrapy.http.cookies import CookieJar
+
+
+class HouseSpiderSpiderMiddleware(object):
+    @classmethod
+    def from_crawler(cls, crawler):
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_input(self, response, spider):
+        return None
+
+    def process_spider_output(self, response, result, spider):
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class HouseSpiderDownloaderMiddleware(object):
+    @classmethod
+    def from_crawler(cls, crawler):
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        s.cookie_jar = CookieJar()
+
+        return s
+
+    def process_request(self, request, spider):
+        request.headers[
+            'referer'] = 'http://search.fang.com/captcha-verify/redirect?h={}'.format(
+                request.url)
+        return None
+
+    def process_response(self, request, response, spider):
+        return response
+
+    def process_exception(self, request, exception, spider):
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class ProxyMiddleware(object):
+    def process_request(self, request, spider):
+        request.meta['proxy'] = 'http://127.0.0.1:8888'
